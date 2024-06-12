92d94b2cf89fadb157b25aeb29363065082f9d29
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-12 09:05:59 +0200

    minor-changes

--- chapter_18/movie-reviews/frontend/src/components/movie.js
+++ chapter_18/movie-reviews/frontend/src/components/movie.js
@@ -7,7 +7,7 @@ import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import moment from 'moment'
+
 
 const Movie = props => {
   const [movie, setMovie] = useState({
@@ -55,28 +55,6 @@ const Movie = props => {
             <br></br>
             <h2>Reviews</h2>
             <br></br>
-            {movie.reviews.map((review, index) => {
-              return (
-                < Card key={index} >
-                  <Card.Body>
-                    <h5>{review.name + " reviewed on "}{" "}{moment(review.date).format("Do MMMM YYYY")}</h5> 
-                    <p>{review.review}</p>
-                    {props.user && props.user.id === review.user_id &&
-                      <Row>
-                        <Col><Link to={{
-                          pathname: "/movies/" +
-                            props.match.params.id +
-                            "/review",
-                          state: { currentReview: review }
-                        }}>Edit</Link>
-                        </Col>
-                        <Col><Button variant="link">Delete</Button></Col>
-                      </Row>
-                    }
-                  </Card.Body>
-                </Card>
-              )
-            })};
           </Col>
         </Row>
       </Container>
