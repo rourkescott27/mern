88cea8259a59d1744f2f03ab7b8f4db83ec6c191
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-13 11:57:56 +0200

    minor-changes

--- chapter_06/movie-reviews/backend/api/movies.route.js
+++ chapter_06/movie-reviews/backend/api/movies.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const router = express.Router() // get access to express router 
+const router = express.Router() 
 router.route('/').get((req, res) => res.send('hello world'));
 
 export default router; 
