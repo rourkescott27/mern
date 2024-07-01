d566f401ab4446ae3613ee166a19223a7e9db16d
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-07-01 09:08:07 +0200

    minor-changes

--- chapter_24/movie-reviews/frontend/src/components/login.js
+++ chapter_24/movie-reviews/frontend/src/components/login.js
@@ -3,20 +3,24 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const Login = props => {
-  const [name, setName] = useState("")
-  const [id, setId] = useState("")
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+
   const onChangeName = e => {
     const name = e.target.value
     setName(name);
-  }
+  };
+
   const onChangeId = e => {
     const id = e.target.value
     setId(id);
-  }
+  };
+
   const login = () => {
     props.login({ name: name, id: id })
     props.history.push('/')
-  }
+  };
+  
   return (
     <div>
       <Form>
